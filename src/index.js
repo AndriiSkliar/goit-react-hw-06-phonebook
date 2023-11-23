b80c2d110ebcdbd7baadbf4b2f31cff97ac42b6a
Андрей Скляр <andreas.skliar@gmail.com>
2023-11-23 20:09:44 +0200

    the end

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import './index.css';
-import { store } from 'redux/store';
+import { persistor, store } from 'redux/store';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/goit-react-hw-06-phonebook">
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,37 @@
-import { combineReducers, createStore } from "redux";
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { productsReducer } from "./products/products.reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const rootReducer = combineReducers({
-  productsStore: productsReducer,
-})
+import { contactsReducer } from "./contacts/contacts.reducer";
+import { modalReducer } from "./modal/modal.reducer";
 
-const enhancer = devToolsEnhancer();
+const contactsConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['contacts'],
+  // blacklist: ['isLoading', 'error'],
+};
 
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: {
+    contactsStore: persistReducer(contactsConfig, contactsReducer),
+    modal: modalReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
